e0bbad026c291b75b0c09f117b7a46ef75b07872
hannahre <hannahreandrews@gmail.com>
2019-11-19 15:42:49 -0700

    Renaming Variables MIDUS1 in R

--- R/coding-MIDUS1.R
+++ R/coding-MIDUS1.R
@@ -3,14 +3,33 @@
 # hannahreandrews@gmail.com 
 
 setwd("C:/Users/hanna/git/AHL/R")
+library(tidyverse)
 
 ## Commands for exploring data frame 
 # head(da02760.0001)
 # str(da02760.0001)
 # dim(da02760.0001)
 # summary(da02760.0001)
+#colnames(MIDUS1A)
 
 load (file = 'MIDUS1/DS0001/02760-0001-Data.rda')
+MIDUS1A <- da02760.0001
+glimpse(MIDUS1A) 
 
-#Run ICPSR supplemental file factor_to_numeric_ICPSR
-source('MIDUS1/factor_to_numeric_icpsr.R')
+#Rename Variables
+# MIDUS1 = 1 at the end, MIDUS2 = 2, MIDUS3 = 3
+MIDUS1A %>% 
+  rename(
+    M2ID = id,
+    M2FAMNUM = famNum,
+    SAMPLMAJ = majorSample,
+    A1STATUS = completion1,
+    A1PAGE_M2 = age1,
+    A1PBYEAR_2019 = birthYear, 
+    A1PRSEX = female, 
+    A1PA4 = selfEvalPhysicalHealth1
+    A1PA5 = selfEvalMentalHealth1
+    
+    
+  )
+
